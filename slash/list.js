6c3f77ea3711f1e79bdd38f1e90b3b05d616a932
julianfere_ <julianferegotti96@gmail.com>
2022-10-29 16:12:02 -0300

    list all emojis command

---
+++ slash/list.js
@@ -0,0 +1,23 @@
+exports.run = async (client, interaction) => {
+  // eslint-disable-line no-unused-vars
+  await interaction.deferReply();
+  const reply = await interaction.channel.guild.emojis.fetch();
+
+  const emojis = reply.map((e) => `<a:${e.name}:${e.id}>`).join(" ");
+
+  await interaction.editReply(emojis);
+};
+
+exports.commandData = {
+  name: "list",
+  description: "list emojis",
+  options: [],
+  defaultPermission: true,
+};
+
+// Set guildOnly to true if you want it to be available on guilds only.
+// Otherwise false is global.
+exports.conf = {
+  permLevel: "User",
+  guildOnly: false,
+};
