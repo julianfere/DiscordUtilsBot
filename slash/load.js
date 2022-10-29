bb09b979b7af726815228704481e92b8283f6a02
julianfere_ <julianferegotti96@gmail.com>
2022-10-29 15:55:14 -0300

    initial commit

---
+++ slash/load.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const path = require("path");
+
+function normalizeName(name) {
+  return path.basename(name).split(".")[0];
+}
+
+function getEmojisPaths() {
+  let p = path.resolve("./emojis");
+  let files = fs.readdirSync(p);
+
+  return files.map((f) => `./emojis/${f}`);
+}
+
+exports.run = async (client, interaction) => {
+  await interaction.deferReply();
+  const channel = interaction.channel;
+  const guild = channel.guild;
+  const emojis = getEmojisPaths().splice(0, 50);
+  try {
+    const responeMessage = [];
+    interaction.editReply("Loading...");
+    for (const emoji of emojis) {
+      const emo = await guild.emojis.create(emoji, normalizeName(emoji));
+      responeMessage.push(`<a:${emo.name}:${emo.id}>`);
+      console.log(`Loaded ${emo.name}`);
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await interaction.editReply("Done!");
+    channel.send(responeMessage.join(" "));
+  }
+};
+
+exports.commandData = {
+  name: "load",
+  description: "load emojis",
+  options: [],
+  defaultPermission: true,
+};
+
+// Set guildOnly to true if you want it to be available on guilds only.
+// Otherwise false is global.
+exports.conf = {
+  permLevel: "Owner",
+  guildOnly: false,
+};
